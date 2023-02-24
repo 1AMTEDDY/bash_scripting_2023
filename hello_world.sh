e61c885ad14916aea7a2549d597c7f324798416c
Peterson Henderson <teddy@teddys-Pro.local>
2023-02-24 22:50:44 +0100

    hello world

---
+++ hello_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#bash typically works its way down.  It will execute the first line, then the second, then the third, etc.
+#The first line is called a shebang.  It tells the system that this is a bash script.
+#AUTOMATE EVERYTHING!!!, THAT IS THE VALUE OF SCRIPTING.
+
+echo 'Hello World' #echo is a command that prints to the screen.  It is a built in command in bash.
