58d4b904b6dc30b3f5983d1a34adef466d0bbce3
Peterson Nwoko <petersonwoko@gmail.com>
2023-03-03 17:45:41 +0100

    math.sh

---
+++ math_func.sh
@@ -0,0 +1,7 @@
+# expr is used to tell bash that this is an expression.  You can use expr to do math.  You can also use the $(( )) syntax to do math.  The $(( )) syntax is preferred because it is more readable.
+# * isnt multiplication, it is a wildcard.  You have to use the x for multiplication. or escape the character like \*
+
+expr 5 + 4
+echo $(( 5 + 4 ))
+mynum1=5
+$(( mynum1 + 4 )) #you can do math functions to variables.
